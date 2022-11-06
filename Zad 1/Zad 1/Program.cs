e231ae84095ca3671c0f750a637b3368364db36c
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 20:45:36 +0100

    Add files via upload

---
+++ Zad 1/Zad 1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            
+            Console.WriteLine("Podaj imie");
+
+            imie = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + imie);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
